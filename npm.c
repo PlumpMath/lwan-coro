a8dbbd93234767784e2819befe0f2766e42016ad
Soojin Nam <jsunam@gmail.com>
2017-04-08 08:22:38 +0900

    Non-Preemptive Multithreading

--- npm.c
+++ npm.c
@@ -19,7 +19,6 @@ struct url_info {
     apr_pool_t *p;
     const char *host;
     const char *path;
-    apr_ssize_t rv;
 };
 
 
@@ -33,11 +32,9 @@ APR_RING_HEAD(_coro_ring_t, _coro_elem_t);
 
 
 static int download(coro_t *coro);
-static int do_client_task(apr_socket_t *sock, coro_t *coro);
 static apr_status_t do_connect(apr_socket_t **sock, coro_t *coro);
 static void get(coro_switcher_t *switcher, coro_ring_t *threads,
                 const char *host, const char *path, apr_pool_t *mp);
-static void dispatch(coro_ring_t *threads);
 
 
 int
@@ -54,38 +51,34 @@ main(int argc, const char *argv[])
     threads = apr_palloc(mp, sizeof(coro_ring_t));
     APR_RING_INIT(threads, _coro_elem_t, link);
 
-    get(&switcher, threads, "www.w3.org", "/TR/html401/html40.txt", mp);
-    get(&switcher, threads, "www.w3.org", "/TR/2002/REC-xhtml1-20020801/xhtml1.pdf", mp);
-    get(&switcher, threads, "www.w3.org", "/TR/REC-html32.html", mp);
-    get(&switcher, threads, "www.w3.org", "/TR/2000/REC-DOM-Level-2-Core-20001113/DOM2-Core.txt", mp);    
+    get(&switcher, threads, "www.w3.org",
+        "/TR/html401/html40.txt", mp);
+    get(&switcher, threads, "www.w3.org",
+        "/TR/2002/REC-xhtml1-20020801/xhtml1.pdf", mp);
+    get(&switcher, threads, "www.w3.org",
+        "/TR/REC-html32.html", mp);
+    get(&switcher, threads, "www.w3.org",
+        "/TR/2000/REC-DOM-Level-2-Core-20001113/DOM2-Core.txt", mp);    
     
-    dispatch(threads);
-
-    apr_terminate();
-
-    return 0;
-}
-
-
-static void
-dispatch(coro_ring_t *threads)
-{
-    int rv;
-    const coro_elem_t *ep;
-    
+    /* dispatch */
     while (1) {
+        const coro_elem_t *ep;
         if (APR_RING_EMPTY(threads, _coro_elem_t, link))
             break;
         
         for (ep = APR_RING_FIRST(threads);
              ep != APR_RING_SENTINEL(threads, _coro_elem_t, link);
              ep = APR_RING_NEXT(ep, link)) {
-            coro_t *coro = ep->coro;
-            if ((rv = coro_resume(coro)) != CORO_MAY_RESUME) {
+            if (coro_resume(ep->coro) != CORO_MAY_RESUME) {
                 APR_RING_REMOVE(ep, link);
+                //coro_free(coro);
             }
         }
     }
+
+    apr_terminate();
+
+    return 0;
 }
 
 
@@ -109,31 +102,51 @@ int download(coro_t *coro)
 {
     apr_status_t rv;
     apr_socket_t *s;
-    int rc;
+    struct url_info *uinfo = (struct url_info *) coro_get_data(coro);
+    apr_pool_t *mp = uinfo->p;
+    const char *req_hdr = apr_psprintf(mp, "GET %s HTTP/1.0"CRLF_STR CRLF_STR,
+                                       uinfo->path);
+    apr_size_t len = strlen(req_hdr);
+    char buf[BUFSIZE];
+    apr_ssize_t total = 0;
     
     rv = do_connect(&s, coro);
     if (rv != APR_SUCCESS) {
         goto error;
     }
+
+    coro_defer(coro, CORO_DEFER(apr_socket_close), &s);
     
-    rc = do_client_task(s, coro);
-    if (rc == CORO_ABORT) {
-        return CORO_ABORT;
+    rv = apr_socket_send(s, req_hdr, &len);
+    if (rv != APR_SUCCESS) {
+        goto error;
     }
-    apr_socket_close(s);
 
-    return CORO_FINISHED;
+    while (1) {
+        len = sizeof(buf);
+        rv = apr_socket_recv(s, buf, &len);
+        if (APR_STATUS_IS_EAGAIN(rv)) {
+            coro_yield(coro, CORO_MAY_RESUME);
+        } else if (APR_STATUS_IS_EOF(rv) || len == 0) {
+            break;
+        } else {
+            total += len;
+        }
+    }
+    printf("%s:\t%zd\n", uinfo->path, total);
 
+    coro_yield(coro, CORO_FINISHED);
+    __builtin_unreachable();
+    
 error:
     {
         char errbuf[256];
         apr_strerror(rv, errbuf, sizeof(errbuf));
         printf("error: %d, %s\n", rv, errbuf);
     }
-
+    
     coro_yield(coro, CORO_ABORT);
     __builtin_unreachable();
-    return CORO_ABORT;
 }
 
 
@@ -146,7 +159,8 @@ do_connect(apr_socket_t **sock, coro_t * coro)
     struct url_info *uinfo = (struct url_info *) coro_get_data(coro);
     apr_pool_t *mp = uinfo->p;
     
-    rv = apr_sockaddr_info_get(&sa, uinfo->host, APR_INET, DEF_REMOTE_PORT, 0, mp);
+    rv = apr_sockaddr_info_get(&sa, uinfo->host,
+                               APR_INET, DEF_REMOTE_PORT, 0, mp);
     if (rv != APR_SUCCESS) {
         return rv;
     }
@@ -172,44 +186,3 @@ do_connect(apr_socket_t **sock, coro_t * coro)
     
     return APR_SUCCESS;
 }
-
-
-static int
-do_client_task(apr_socket_t *sock, coro_t *coro)
-{
-    apr_status_t rv;
-    struct url_info *uinfo = (struct url_info *) coro_get_data(coro);
-    apr_pool_t *mp = uinfo->p;
-    const char *req_hdr = apr_pstrcat(mp, "GET ", uinfo->path, " HTTP/1.0" CRLF_STR CRLF_STR, NULL);
-    apr_ssize_t total = 0;
-    apr_size_t len = strlen(req_hdr);
-
-    
-    rv = apr_socket_send(sock, req_hdr, &len);
-    if (rv != APR_SUCCESS) {
-        uinfo->rv = -1;
-        coro_yield(coro, CORO_ABORT);
-        __builtin_unreachable();
-    }
-
-    while (1) {
-        char buf[BUFSIZE];
-        apr_size_t len = sizeof(buf);
-        
-        apr_status_t rv = apr_socket_recv(sock, buf, &len);
-        if (APR_STATUS_IS_EAGAIN(rv)) {
-            coro_yield(coro, CORO_MAY_RESUME);
-        } else if (APR_STATUS_IS_EOF(rv) || len == 0) {
-            break;
-        } else {
-            total += len;
-        }
-    }
-
-    uinfo->rv = total;
-
-    printf("%s: %zd\n", uinfo->path, total);
-    
-    return CORO_FINISHED;
-}
-
