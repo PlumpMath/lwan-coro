a0bfdaeff4abd8c9a4db28a93381d0014604d911
Donald Nam <donald.knuth@as-MacBook-Pro-6.local>
2017-04-11 14:15:36 +0900

    permutation with coroutine

---
+++ permgen.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <lwan-coro.h>
+
+
+struct perma {
+    int *a;
+    int n;
+};
+
+
+static void
+print_result(int *a, int n)
+{
+    int i;
+    
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+
+static void swap(int *x, int *y)
+{
+    int t;
+
+    t = *x;
+    *x = *y;
+    *y = t;
+}
+
+
+static int
+permgen(coro_t *coro)
+{
+    struct perma *pa = (struct perma *) coro_get_data(coro);
+    int *a = pa->a;
+    int n = pa->n;
+    
+    if (n <= 0) {
+        coro_yield(coro, CORO_MAY_RESUME);
+    } else {
+        int i;
+        for (i = 0; i < n; i++) {
+            swap(a+n-1, a+i);
+            pa->n = n - 1;
+            permgen(coro);
+            swap(a+n-1, a+i);
+        }
+    }
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    coro_switcher_t switcher;
+    int a[] = { 1, 2, 3, 4 };
+    int n = sizeof(a)/sizeof(int);
+    struct perma pa = { a, n };
+    coro_t *coro = coro_new(&switcher, permgen, &pa);
+
+    while (1) {
+        struct perma *pa;
+        
+        if (coro_resume(coro))
+            break;
+        
+        pa = (struct perma *) coro_get_data(coro);
+        print_result(pa->a, n);
+    }
+
+    return 0;
+}
